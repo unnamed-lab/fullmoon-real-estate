d9adcab75bfe6aa867b537636ec45c52cfeb16c2
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2024-02-18 16:56:54 +0100

    hero section ui - homepage

--- src/components/ui/Nav.jsx
+++ src/components/ui/Nav.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import "../../styles/css/section/navbar.css";
+import logo from "../../assets/icons/logo.png";
+import { Link } from "react-router-dom";
 
 function Nav() {
+  const navMenu = [
+    { title: "home", url: "/" },
+    { title: "about", url: "/about" },
+    { title: "buy", url: "/" },
+    { title: "services", url: "/service" },
+    { title: "contact", url: "/contact" },
+  ];
   return (
-    <div>Nav</div>
-  )
+    <header>
+      <nav className="nav">
+        <Link to={"/"} className="brand_logo">
+          <img src={logo} alt="Fullmoon Real Estate" />
+        </Link>
+        <ul className="nav_menu">
+          {navMenu?.map((el, index) => {
+            return (
+              <li key={index} className="nav_option">
+                <Link to={el.url}>{el.title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default Nav
+export default Nav;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Hero, Service } from "../components/home";
+import bgImg from "../assets/img/banner01.png";
+import { SearchField } from "../components/forms";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <Hero bgImg={bgImg}>
+        <h1>Build your dream home on the finest property</h1>
+        <SearchField />
+      </Hero>
+      <Service />
+    </>
+  );
 }
 
-export default Home
+export default Home;
