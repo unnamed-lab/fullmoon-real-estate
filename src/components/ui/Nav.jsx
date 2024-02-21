50b2e2f937dcd1009ddc1c247b716352fbf0069c
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2024-02-21 01:53:25 +0100

    navbar and search form tweak -homepage

--- src/components/ui/Nav.jsx
+++ src/components/ui/Nav.jsx
@@ -1,8 +1,10 @@
 import "../../styles/css/section/navbar.css";
 import logo from "../../assets/icons/logo.png";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Nav() {
+  const [activeNav, setNav] = useState(false);
   const navMenu = [
     { title: "home", url: "/" },
     { title: "about", url: "/about" },
@@ -16,7 +18,29 @@ function Nav() {
         <Link to={"/"} className="brand_logo">
           <img src={logo} alt="Fullmoon Real Estate" />
         </Link>
-        <ul className="nav_menu">
+        <button
+          className="nav_toggle"
+          onClick={() => {
+            setNav(!activeNav);
+          }}
+        >
+          <svg
+            width="26"
+            height="26"
+            viewBox="0 0 26 26"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M3.25 6.5H22.75M3.25 13H22.75M3.25 19.5H22.75"
+              stroke="white"
+              strokeWidth="2.8"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
+        <ul className={`nav_menu ${activeNav ? "show" : ""}`}>
           {navMenu?.map((el, index) => {
             return (
               <li key={index} className="nav_option">
@@ -25,6 +49,10 @@ function Nav() {
             );
           })}
         </ul>
+        <div
+          className={`nav_menu_backdrop ${activeNav ? "" : "hide"}`}
+          onClick={() => setNav(!activeNav)}
+        ></div>
       </nav>
     </header>
   );
