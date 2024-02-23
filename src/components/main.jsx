233a52264f8171c0f0442c26a2e1551e05f5ef01
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2024-02-23 17:57:57 +0100

    SEO config tweak #05

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
+import { hydrateRoot, createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "../styles/css/root.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+
+const app = (
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(rootElement, app)
+} else {
+  createRoot(rootElement).render(app);
+}
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
