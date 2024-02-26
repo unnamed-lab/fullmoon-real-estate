b88edadda1b2bb3668b86cbcced9edd184c681bd
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2024-02-26 07:56:35 +0100

    prerender.io config

--- server.cjs
+++ server.cjs
@@ -4,11 +4,13 @@ const app = express();
 require("dotenv").config();
 
 app.use(
-  require("prerender-node").set("prerenderToken", process.env.PREREDNER_TOKEN)
+  require("prerender-node").set("prerenderToken", process.env.PRERENDER_TOKEN)
 );
 
-// const port = process.env.PORT || 5000;
 
-// app.listen(port, () => {
-//   console.log(`Prerender running...`);
-// });
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Prerender running...`);
+});
